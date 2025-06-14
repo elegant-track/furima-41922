e3f2856b5a321d260c6422c0ce6dd8b88071e86a
elegant-track <nabe.2626.mphb@icloud.com>
2025-06-15 00:33:45 +0900

    バリデーションとrspecの準備

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,19 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name_kana, presence: true
-  validates :first_name_kana, presence: true
-  validates :birthday, presence: true
+  with_options presence: true do
+    validates :nickname
+    validates :birthday
 
+    validates :first_name_kana, format: { with: /\A[ァ-ヶー]+\z/, message: 'カタカナで入力してください' }
+    validates :last_name_kana,  format: { with: /\A[ァ-ヶー]+\z/, message: 'カタカナで入力してください' }
+
+    with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
+      validates :first_name
+      validates :last_name
+    end
+  end
   
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください', allow_nill: true
 end
