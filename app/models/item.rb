b23d5af3d50187a7dcd073387c071ad05b078aee
elegant-track <nabe.2626.mphb@icloud.com>
2025-06-17 20:36:23 +0900

    ActiveHashの実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,19 @@
 class Item < ApplicationRecord
   has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :shopping_fee
+  belongs_to :prefecture
+  belongs_to :delivery_time
+
+  validates :name, presence: true
+  validates :explanation, presence: true
+  validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :status_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :shopping_fee_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :delivery_time_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :price, presence: true
+  validates :image, presence: true
 end
