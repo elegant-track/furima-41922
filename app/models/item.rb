f7314a0a4c0c61817fc48816a49a6781b6a16848
elegant-track <nabe.2626.mphb@icloud.com>
2025-06-23 21:27:45 +0900

    テストコードの実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -14,6 +14,6 @@ class Item < ApplicationRecord
   validates :shopping_fee_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :delivery_time_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :price, presence: true
+  validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
   validates :image, presence: true
 end
--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,20 @@
 FactoryBot.define do
   factory :item do
-    
+    name {'商品名'}
+    explanation {'説明文'}
+    category_id { 2 }
+    status_id { 2 }
+    shopping_fee_id { 2 }
+    prefecture_id { 2 }
+    delivery_time_id { 2 }
+    price { 1000 }
+
+    after(:build) do |item|
+      item.image.attach(
+        io: File.open(Rails.root.join('spec/fixtures/sample.jpg')),
+        filename: 'sample.jpg',
+        content_type: 'image/jpeg'
+      )
+    end
   end
 end
